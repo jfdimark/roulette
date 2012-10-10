f9e7adc3c4ea43f22c2a2847147b7c572598d3ad
Mark Walker <m_e_walker@hotmail.com>
2012-10-10 19:30:46 +0100

    file structure

---
+++ lib/Spin.rb
@@ -0,0 +1,8 @@
+
+# The Shuffleable module contains methods that enable randomly shuffling an array
+module SpinWheel
+  def spin_wheel(wheel)
+	# shuffle! is a built in array method
+	# could substitute an arbitrary shuffling algorithm here
+    return wheel.shuffle!
+  end
---
+++ roulette.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+# require game class from /lib relative to current directory per modified LOAD_PATH
+require "lib/game"
