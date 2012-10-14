2eb11f9fbf809fb068cc0601b806715765cc5523
brewingideas <sean@brewingideas.co.uk>
2012-10-14 16:58:30 +0100

    user

--- lib/User.rb
+++ lib/User.rb
@@ -1,1 +1,19 @@
-User
+class User
+
+	attr_accessor :name
+	attr_accessor :hand
+	attr_accessor :total
+
+	def initialize(name, hand)
+		@name = name
+		@hand = Array.new
+		@total = 100
+		@bust = faulse
+	end
+
+	
+
+
+
+
+
--- lib/Wheel.rb
+++ lib/Wheel.rb
@@ -1,1 +1,5 @@
 Wheel
+
+#the wheel should take the attributes of numbers
+#give an output when spun
+#output should be random
